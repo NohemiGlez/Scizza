a6a0874ed7ceb8fa25f72e953b1c8c0c3824afa5
Irvin Lara <a305047@uach.mx>
2018-11-18 20:26:46 -0700

    Agrega el modelo developerTeam.js

---
+++ models/developerTeam.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const mongoosePaginate = require('mongoose-paginate-v2');
+const Schema = mongoose.Schema;
+
+const schema = Schema({
+  _id: ObjectId,
+  _teamName: String
+});
+
+class DeveloperTeam{
+  constructor(id, teamName){
+    this._id = id;
+    this._teamName = teamName;
+  }
+
+  get id(){
+    return this._id;
+  }
+
+  set id(v){
+    this._id = v;
+  }
+
+  get teamName(){
+    return this._teamName;
+  }
+
+  set teamName(v){
+    this._teamName = v;
+  }
+
+}
+
+schema.plugin(mongoosePaginate);
+schema.loadClass(DeveloperTeam);
+module.exports = mongoose.model('DeveloperTeam', schema);
