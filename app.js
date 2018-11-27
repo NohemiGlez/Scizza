86c82395f5531e9dacc311a678183879e682b492
Raul Villarreal <raulvilla0797@gmail.com>
2018-11-27 12:16:35 -0700

    Agrega passport a la aplicacion

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const passportSetup = require('./config/passportSetup');
 
 const indexRouter = require('./routes/index');
 const profileRouter = require('./routes/profile');
