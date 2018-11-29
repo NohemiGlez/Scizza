903b60d1792278467aeadb0293b70d11522a1a1d
Nohemi Gonzalez <ngonzalez.swe@gmail.com>
2018-11-28 17:52:29 -0700

    Enlaza perfil y cerrar sesión en dashboard

--- routes/userSkills.js
+++ routes/userSkills.js
@@ -4,6 +4,10 @@ const userSkillsController = require('../controllers/userSkillsController');
 
 router.post('/', userSkillsController.create);
 
+router.get('/', (req, res) => {
+    res.render('userSkills', { title: 'Scizza | Perfil de usuario', username: req.user, principalSkill: 'Desarrollador Web' });
+})
+
 router.get('/get', userSkillsController.listAll);
 
 router.get('/show/:id?', userSkillsController.listOne);
