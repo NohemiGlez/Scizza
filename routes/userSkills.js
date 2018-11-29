77fc0b3a3548a195175002c22f0e4b884775c2d5
Nohemi Gonzalez <ngonzalez.swe@gmail.com>
2018-11-28 18:03:45 -0700

    Agrega usersSkills a rutas y a dashboard

--- routes/userSkills.js
+++ routes/userSkills.js
@@ -2,10 +2,18 @@ let express = require('express');
 let router = express.Router();
 const userSkillsController = require('../controllers/userSkillsController');
 
+const authCheck = (req, res, next) => {
+    if (!req.user) {
+        res.redirect('/');
+    } else {
+        next();
+    }
+};
+
 router.post('/', userSkillsController.create);
 
-router.get('/', (req, res) => {
-    res.render('userSkills', { title: 'Scizza | Perfil de usuario', username: req.user, principalSkill: 'Desarrollador Web' });
+router.get('/', authCheck, (req, res) => {
+    res.render('userSkills', { title: 'Scizza | Habilidades de usuario', username: req.user, principalSkill: 'Desarrollador Web' });
 })
 
 router.get('/get', userSkillsController.listAll);
