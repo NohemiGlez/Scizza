2cb9ad6445e9492678afa2bdbd420357c1a05c67
Nohemi Gonzalez <ngonzalez.swe@gmail.com>
2018-11-29 01:08:47 -0700

    Agrega vista de equipos

--- routes/developerTeams.js
+++ routes/developerTeams.js
@@ -2,7 +2,19 @@ let express = require('express');
 let router = express.Router();
 const developerTeamsController = require('../controllers/developerTeamsController');
 
+const authCheck = (req, res, next) => {
+    if (!req.user) {
+        res.redirect('/');
+    } else {
+        next();
+    }
+};
+
 router.post('/', developerTeamsController.create);
+
+router.get('/', authCheck, (req, res) => {
+    res.render('developerTeams/listAll', { title: 'Scizza | Habilidades de usuario', username: req.user, principalSkill: 'Desarrollador Web' });
+});
 
 router.get('/get', developerTeamsController.listAll);
 
