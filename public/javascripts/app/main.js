7a47a23897228aaf0861a00f8fbd1b667a302cb3
Nohemi Gonzalez <ngonzalez.swe@gmail.com>
2018-11-29 17:06:46 -0700

    Agrega tarjetas sin drag

--- public/javascripts/app/main.js
+++ public/javascripts/app/main.js
@@ -1,42 +1,39 @@
-const app = new Vue({
-    el:'#app',
-    data:{
-        // Objetos que contengan información de la app (modelos)
-        users: [ 
-        ],
-        user:{
-            name: 'Luis',
-            lastName: 'Ramírez'
-        },
-        operators:{
-            n1: 0,
-            n2: 0,
-            res: 0
+Vue.use(VueDraggable);
+
+new Vue({
+    data: {
+        options: {
+            dropzoneSelector: 'ul',
+            draggableSelector: 'li',
+            excludeOlderBrowsers: true,
+            showDropzoneAreas: true,
+            multipleDropzonesItemsDraggingEnabled: true,
+            onDrop(event) {},
+            onDragstart(event) {
+              event.stop();
+            },
+            onDragend(event) {
+              event.stop();
+            }
         }
     },
-    methods:{
-        // Todas las funciones comunes de la app
-        // Es un método que tiene que ser llamado para ejecutarse
-        sum: function(event){
-            this.operators.res = this.operators.n1 + this.operators.n2;
-        }
-    },
-    computed: {
-        // Actualiza solo, siempre se está ejecutando, se vuelve una propiedad virtual
-        // Siempre tiene que regresar un resultado, si no, no mostraría nada
-        result(){
-            return this.operators.n1 + this.operators.n2;
-        }
-        // Funciones que podrán ser 
-        // desplegables en las vistas sólo si regresan un resultado.
-        
-    },
-    created(){
-        // Aquí se ejecuta código al inicializar la aplicación.
-        fetch('/users/get/')
-        .then(response => response.json())
-        .then(json => {
-            this.users = json.data.docs;
-        });
-    }
-});
+    template: `
+  <div drag-and-drop:options="options" class="drag-wrapper">
+    <ul>
+      <li>Item 1</li>
+      <li>Item 2</li>
+      <li>Item 3</li>
+    </ul>
+    <ul>
+      <li>Item 4</li>
+      <li>Item 5</li>
+      <li>Item 6</li>
+    </ul>
+    <ul>
+      <li>Item 7</li>
+      <li>Item 8</li>
+      <li>Item 9</li>
+    </ul>
+  </div>
+  `
+}).$mount("#app");
