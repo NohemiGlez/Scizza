7c5933c73eea67065eb2dacdaa5f1479aaece925
Nohemi Gonzalez <ngonzalez.swe@gmail.com>
2018-11-30 00:41:48 -0700

    Modifica rutas y modal de la vista

--- routes/userStories.js
+++ routes/userStories.js
@@ -2,7 +2,19 @@ let express = require('express');
 let router = express.Router();
 const userStoryController = require('../controllers/userStoryController');
 
+const authCheck = (req, res, next) => {
+    if (!req.user) {
+        res.redirect('/');
+    } else {
+        next();
+    }
+};
+
 router.post('/', userStoryController.create);
+
+router.get('/', authCheck, (req, res) => {
+    res.render('userStories', { title: 'Scizza | Historias de usuario', username: req.user, principalSkill: 'Desarrollador Web' });
+})
 
 router.get('/get', userStoryController.listAll);
 
